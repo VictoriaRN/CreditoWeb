01882517328a55f4fe04b218e6dd7a5a246680f0
VictoriaRN <victoria.reynoso@alumnos.udg.mx>
2018-03-13 06:28:25 -0600

    CredtWeb
    Practica 4

--- Models/Tarjeta.cs
+++ Models/Tarjeta.cs
@@ -1,67 +1,79 @@
 using System;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
 
 namespace CreditoWeb.Models
 {
     public class Tarjeta
     {
         [Required(ErrorMessage = "El número de tarjeta es necesario.")]
-        [CreditCard]
+        //[CreditCard]
         public string TarjetaNum { get; set; }
         public TipoTarjeta TipoTarjeta { get; set; }
 
         public bool Valida { get; set; }
-
+     
         public Tarjeta(string tarjetaNum)
         {
             this.TarjetaNum = tarjetaNum;
             Valida = esValida();
-            TipoTarjeta = tipoDeTarjeta();
+            TipoTarjeta = tipoDeTarjeta();            
         }
-
 
         /// Basado en el algoritmo de Luhn determinar si esta tarjeta es valida
         /// como estamos dentro de la clase de tarjeta tenemos acceso a la propiedad TarjetaNum 
         private bool esValida()
         {
-            return TarjetaNum.Reverse() // ordena el número de la tarjeta al revés 
-                               .Select(c => char.GetNumericValue(c)) // convierte los caracteres a un valor numérico
-                               .Select((num, index) => index % 2 == 0 ? num : ((num *= 2) > 9 ? num - 9 : num)) // si la posición es par lo multiplica por dos y suma los digitos 
-                               .Sum() % 10 == 0; // suma todos los elementos de la lista y pregutna si es multiplo de 10 sacando el residuo de la operación
+           if(TarjetaNum.Length>=14&&TarjetaNum.Length<=17){
+            string numeroF1, numeroF2;
+            int numEntF1, numEntF2;
+            int reMOD, reNOMO = 0;
+            int sumf1 = 0, sumFNL = 0;
+     
+            for(int i=TarjetaNum.Length-2; i==0; i-=2){
+                numeroF1= TarjetaNum.Substring(i,1);
+                numEntF1 = Convert.ToInt32(numeroF1);
+                reMOD = numEntF1*2;
+                if(reMOD>9){
+                    reMOD = reMOD-9;
+                }
+                sumf1 = reMOD + sumf1; 
+                }
+                
+            for(int c=TarjetaNum.Length-1; c==0; c-=2){
+                numeroF2= TarjetaNum.Substring(c,1);
+                numEntF2 = Convert.ToInt32(numeroF2);
+              reNOMO = numEntF2 + reNOMO;
+            }
+          
+          sumFNL = reNOMO + sumf1;
+        Valida = (sumFNL%10==0);
+             
+          
+        
+        
+        
 
         }
-
+        return Valida;
+        }
 
         /// Si la tarjeta es valida determinar de cuál tipo es VISA, MASTERCARD, AMERICANEXPRESS
         /// como estamos dentro de la clase de tarjeta tenemos acceso a la propiedad TarjetaNum 
         private TipoTarjeta tipoDeTarjeta()
         {
-            if (Valida)
-            {
-                var longitud = TarjetaNum.Length;
-                var primerosDos = TarjetaNum.Substring(0, 2);
-
-                if (longitud == 15 && (primerosDos.Equals("34") || primerosDos.Equals("37")))
-                {
-                    return TipoTarjeta.AMERICANEXPRESS;
-                }
-                if (longitud == 16)
-                {
-                    if (primerosDos.Equals("51") || primerosDos.Equals("52") || primerosDos.Equals("53") || primerosDos.Equals("54") || primerosDos.Equals("55"))
-                    {
-                        return TipoTarjeta.MASTERCARD;
-                    }
-                    if (primerosDos.StartsWith("4"))
-                    {
-                        return TipoTarjeta.VISA;
-                    }
-                }
-                return TipoTarjeta.NOVALIDA; // Si es valida pero no es de ningún tipo
+               if(TarjetaNum.Substring(0,1)=="4"){
+                return TipoTarjeta.VISA;
             }
+            if(TarjetaNum.Substring(0,2)=="51"||TarjetaNum.Substring(0,2)=="52"||TarjetaNum.Substring(0,2)=="53"||TarjetaNum.Substring(0,2)=="54"||TarjetaNum.Substring(0,2)=="55"){
+                return TipoTarjeta.MASTERCARD;
+            }
+            if(TarjetaNum.Substring(0,2)=="34"||TarjetaNum.Substring(0,2)=="37"){
+                return TipoTarjeta.AMERICANEXPRESS;
+            }
+           
             return TipoTarjeta.NOVALIDA;
-        }
 
+        }
 
 
     }
@@ -72,7 +84,6 @@ namespace CreditoWeb.Models
         MASTERCARD,
         AMERICANEXPRESS,
         NOVALIDA
-
 
     }
 }
